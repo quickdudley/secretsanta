1b084040bd386eed835720798c2b5f370e7ca740
Jeremy List <quick.dudley@gmail.com>
2017-12-07 22:15:18 +1300

    Disability friendliness

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,11 @@ import Codec.Phaser
 import Codec.Phaser.Common
 import Codec.Phaser.UTF8
 
+data Annotation =
+  NoAnnotation |
+  Already String |
+  Assisted [String]
+
 main :: IO ()
 main = getArgs >>= \args -> case args of
   [] -> hPutStrLn stderr "Usage: secretsanta <filename>"
@@ -28,24 +33,30 @@ main = getArgs >>= \args -> case args of
            hPutStrLn stderr $ "    " ++ em
      Right (m:_) -> do
        rng0 <- getStdGen
-       let (rng1,cm) = completeMatching m rng0
-       setStdGen rng1
-       forM_ (M.toList cm) $ \(s,r) -> writeFile s r
+       let
+         (rng1,cm) = completeMatching m rng0
+         (rng2,fa) = chooseAssistance m cm rng1
+       setStdGen rng2
+       forM_ (M.toList fa) $ \(s,r) -> writeFile (s ++ ".txt") $
+         intercalate "\n" $ map (\(ss,rcp) -> conj ss ++ rcp) $ M.toList r
+ where
+  conj "You" = "You give to "
+  conj n = n ++ " gives to "
 
-completeMatching :: RandomGen g => M.Map String (Maybe String) -> g ->
+completeMatching :: RandomGen g => M.Map String Annotation -> g ->
   (g, M.Map String String)
 completeMatching m0 rng0 = case attemptMatching m0 rng0 of
   (rng1,Nothing) -> completeMatching m0 rng1
   (rng1,Just r) -> (rng1, r)
 
-attemptMatching :: RandomGen g => M.Map String (Maybe String) -> g ->
+attemptMatching :: RandomGen g => M.Map String Annotation -> g ->
   (g,Maybe (M.Map String String))
 attemptMatching sm rng0 = let
   us0 = M.keysSet $
-    M.mapMaybe (\c -> case c of {Nothing -> Just (); _ -> Nothing})
+    M.mapMaybe (\c -> case c of {Already _ -> Nothing; _ -> Just ()})
     sm
-  ur0 = M.keysSet sm S.\\ S.fromList [x | Just x <- M.elems sm]
-  m0 = M.mapMaybe id sm
+  ur0 = M.keysSet sm S.\\ S.fromList [x | Already x <- M.elems sm]
+  m0 = M.mapMaybe (\an -> case an of {Already x -> Just x; _ -> Nothing}) sm
   go rng us ur m = if S.null us || S.null ur
     then (rng, Just m)
     else let
@@ -55,6 +66,29 @@ attemptMatching sm rng0 = let
         then (rng', Nothing)
         else go rng' us' ur' (M.insert s r m)
   in go rng0 us0 ur0 m0
+
+chooseAssistance :: RandomGen g =>
+  M.Map String Annotation -> M.Map String String -> g ->
+  (g, M.Map String (M.Map String String))
+chooseAssistance sm allocs rng0 = let
+  assisted = M.mapMaybe
+    (\a -> case a of {Assisted v -> Just v; _ -> Nothing})
+    sm
+  acc0 = M.mergeWithKey
+    (\s a r -> case a of
+      Assisted _ -> Nothing
+      _ -> Just (M.singleton "You" r)
+     ) (const M.empty) (const M.empty) sm allocs
+  go acc ra rng
+    | M.null ra = (rng,acc)
+    | otherwise = let
+      ((ap,hs'), ra') = M.deleteFindMin ra
+      rp = allocs M.! ap
+      hs = S.delete rp (S.fromList hs')
+      (rng',h,_) = randomSetMember hs rng
+      acc' = M.alter (fmap (M.insert ap rp)) h acc
+      in go acc' ra' rng'
+  in go acc0 assisted rng0
 
 randomSetMember :: (RandomGen g, Ord a) => S.Set a -> g -> (g,a,S.Set a)
 randomSetMember s rng = let
@@ -70,7 +104,7 @@ randomSetMember s rng = let
 
 isSpacing = (&&) <$> isSpace <*> (not . (`elem` "\n\r"))
 
-efile_p :: Monoid p => Phase p Char o (M.Map String (Maybe String))
+efile_p :: Monoid p => Phase p Char o (M.Map String Annotation)
 efile_p = go M.empty where
   go acc = do
     ~(s,r) <- p_p
@@ -78,14 +112,19 @@ efile_p = go M.empty where
     return acc' <|> (char '\n' *> (return acc' <|> go acc'))
   p_p = do
     n <- n_p
-    pure (n,Nothing) <|> (do
+    pure (n,NoAnnotation) <|> (do
       char ':'
       r <- n_p
-      pure (n, Just r)
+      pure (n, Already r)
+     ) <|> (do
+      char '?'
+      r <- sepBy n_p (char '/')
+      pure (n, Assisted r)
      )
   n_p = munch isSpacing *> n1 id id
   n1 ca sa = get >>= \c -> case c of
     ':' -> fail "Unexpected \":\""
+    '?' -> fail "Unexpected \"?\""
     '\\' -> fail "Names containing backslashes wouldn't work on Windows"
     '/' -> fail "Names containing forward slashes wouldn't work on Unix"
     _ | isSpacing c -> pure (ca []) <|> n1 ca (sa . (c:))
